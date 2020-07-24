936319da6203f9ed6ccc49a34ed171029b3ee8ee
daiki27 <daikiit27@gmail.com>
2020-07-24 18:43:40 +0900

    Initial Commit

---
+++ IconDesign/IconDesign.h
@@ -0,0 +1,19 @@
+//
+//  IconDesign.h
+//  IconDesign
+//
+//  Created by 樋口大樹 on 2020/07/24.
+//  Copyright © 2020 樋口大樹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IconDesign.
+FOUNDATION_EXPORT double IconDesignVersionNumber;
+
+//! Project version string for IconDesign.
+FOUNDATION_EXPORT const unsigned char IconDesignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IconDesign/PublicHeader.h>
+
+
